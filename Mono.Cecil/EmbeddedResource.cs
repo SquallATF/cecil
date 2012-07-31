3a894c2e4b6f25e7f34c366877c0478cdba2f16c
de4dot <de4dot@gmail.com>
2012-07-31 12:47:47 +0200

    Add equals and hash code methods

--- Mono.Cecil/EmbeddedResource.cs
+++ Mono.Cecil/EmbeddedResource.cs
@@ -105,5 +105,33 @@ namespace Mono.Cecil {
 
 			return data;
 		}
+
+		public bool _Equals (object obj)
+		{
+			var other = obj as EmbeddedResource;
+			if (other == null)
+				return false;
+			if ((offset == null && other.offset != null) ||
+				(offset != null && other.offset == null))
+				return false;
+			if (data != other.data)
+				return false;
+			if (stream != other.stream)
+				return false;
+
+			return true;
+		}
+
+		public int _GetHashCode ()
+		{
+			int hash = 0;
+			if (offset != null)
+				hash ^= offset.GetHashCode ();
+			if (data != null)
+				hash ^= data.GetHashCode ();
+			if (stream != null)
+				hash ^= stream.GetHashCode ();
+			return hash;
+		}
 	}
 }
